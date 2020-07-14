ed888791f3a99f33852cfee9150626003902ad1e
Drew Michelini <dbmich@umich.edu>
2020-07-14 19:03:25 -0400

    Re-organized the code to be object oriented, and added a README with usage information

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # drewmich_twitter
-Repository for my first project using the Twitter API
+Command line python module that visualizes related terms on twitter using a wordcloud.
+
+This script relies on a file called twitter_cred.py that contains four variables which 
+store the user twitter API keys and tokens in order to access the API. Users should place a file in the same 
+directory called twitter_cred.py in the format:
+    API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+    API_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    ACCESS_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+These variables are abstracted away in order to keep these values private.
+
+When running the script, the first argument is either "save" or "show". 
+    save: saves the wordcloud to the current working directory
+    show: saves the wordcloud to the current working directory, and displays the image
+    
+The second argument is the term to be searched. It is important NOT to start your term with a hashtag (\#). 
+This breaks the program.
+
+
+
+
--- twitter_proj.py
+++ twitter_proj.py
@@ -5,55 +5,78 @@ import tweepy
 from wordcloud import WordCloud
 from PIL import Image
 
-# Sets authentication for twitter api
-auth = tweepy.OAuthHandler(twitter_cred.API_KEY, twitter_cred.API_SECRET)
-auth.set_access_token(twitter_cred.ACCESS_TOKEN, twitter_cred.ACCESS_SECRET)
+class MainApp:
+    def __init__(self):
+        # Sets authentication for twitter api
+        auth = tweepy.OAuthHandler(twitter_cred.API_KEY, twitter_cred.API_SECRET)
+        auth.set_access_token(twitter_cred.ACCESS_TOKEN, twitter_cred.ACCESS_SECRET)
+        self.api = tweepy.API(auth)
+        #self.keyword = sys.argv[1]
+        self.wc = WordCloud(max_font_size=100, width=600, height=300, relative_scaling=.5)
 
-api = tweepy.API(auth)
+    # Takes in a list and puts all elements in a string
+    def listToString(self, s):
 
-keyword = sys.argv[1]
+        str1 = ""
+        for ele in s:
+            str1 += ele + ' '
 
-wc = WordCloud(max_font_size=100, width=600, height=300, relative_scaling=.5)
-
-# Takes in a list and puts all elements in a string
-def listToString(s):
-
-    str1 = ""
-
-    for ele in s:
-        str1 += ele + ' '
-
-    return str1
+        return str1
 
 
-# Retrieves text from tweets based off a keyword, converts to string
-def getStringOfText(query):
+    # Retrieves text from tweets based off a keyword, converts to string
+    def getStringOfText(self, query):
 
-    tweetslst = []
+        tweetslst = []
 
-    for tweet in api.search(q = query, lang = 'en', result_type = 'mixed', tweet_mode = 'extended', count = 20):
-        encoded_string = tweet.full_text.encode("ascii", "ignore")
-        decode_string = encoded_string.decode()
-        tweetlist = decode_string.split(' ')
-        tweetslst = tweetslst + tweetlist
+        for tweet in self.api.search(q = query, lang = 'en', result_type = 'popular', tweet_mode = 'extended', count = 20):
+            encoded_string = tweet.full_text.encode("ascii", "ignore")
+            decode_string = encoded_string.decode()
+            tweetlist = decode_string.split(' ')
+            tweetslst = tweetslst + tweetlist
 
-    # Removes unwanted words
-    res = [x for x in tweetslst if not x.startswith('https')]
-    res1 = [x for x in res if not "http" in x]
-    res2 = [x for x in res1 if not "u2019" in x]
+        # Removes unwanted words
+        res = [x for x in tweetslst if not x.startswith('https')]
+        res1 = [x for x in res if not "http" in x]
+        res2 = [x for x in res1 if not "u2019" in x]
 
-    # Correctly formats
-    strvar = listToString(res2)
-    strvarlow = strvar.lower()
+        # Correctly formats
+        strvar = self.listToString(res2)
+        strvarlow = strvar.lower()
 
-    return strvarlow
+        return strvarlow
 
 
-wc.generate(getStringOfText(keyword))
+    def dispWC(self, keyword):
+        self.wc.generate(self.getStringOfText(keyword))
+        self.wc.to_file('output.png')
+        img = Image.open('output.png')
+        img.show()
 
-wc.to_file('output.png')
-img = Image.open('output.png')
-img.show()
+    def saveWC(self, keyword):
+        self.wc.generate(self.getStringOfText(keyword))
+        self.wc.to_file('output.png')
+
+def main():
+    if (len(sys.argv) != 3):
+        print("Error: execute function with 3 arguments including program name:")
+        print("Example: ./twiter_proj.py show michigan")
+
+    if (sys.argv[1] != "save" and sys.argv[1] != "show"):
+        print("Error: second argument should be either 'save' or 'show'")
+        print("Example: ./twitter_proj.py show michigan")
+
+    cloud = MainApp()
+
+    if (sys.argv[1]) == "save":
+        cloud.saveWC(sys.argv[2])
+    if (sys.argv[1]) == "show":
+        cloud.dispWC(sys.argv[2])
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
